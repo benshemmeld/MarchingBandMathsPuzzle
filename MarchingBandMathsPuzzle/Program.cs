f6d5677b32a0e2934257febe0cfa2d37bdf433bd
Ben Shemmeld <bshemmeld@gmail.com>
2020-08-06 22:30:12 +1000

    Initial commit

---
+++ MarchingBandMathsPuzzle/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarchingBandMathsPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var targetArrangements = 64;
+            var numBandMembers = 0;
+            while(true)
+            {
+                numBandMembers++;
+
+                var possibleArrangements = GetFactors(numBandMembers);
+                if(possibleArrangements.Count == targetArrangements)
+                {
+                    Console.WriteLine($"{numBandMembers} band members has {targetArrangements} different marching options");
+                    var count = 0;
+                    foreach(var arrangment in possibleArrangements)
+                    {
+                        count++;
+                        Console.WriteLine($"{count} {arrangment.Item1} x {arrangment.Item2}");
+                    }
+                    Console.ReadLine();
+                    return;
+                }
+            }
+        }
+
+        private static List<(int, int)> GetFactors(int number)
+        {
+            var factors = new List<(int, int)>();
+            for(var i=1; i<=number; i++)
+            {
+                if(number%i == 0)
+                {
+                    var otherFactor = number / i;
+                    factors.Add((i, otherFactor));
+                }
+            }
+            return factors;
+        }
+    }
+}
